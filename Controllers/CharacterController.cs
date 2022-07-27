e7d9694ba73da2fe6720c784dc38dbbdf6372c99
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-07-27 14:08:57 -0700

    Add characters with a list Add new charcters to the list Add returning the list of characters Created a Http post to send data

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -34,5 +34,13 @@ namespace rpg_game_dotnet.Controllers
             
                 return Ok(characters.FirstOrDefault(c => c.Id == id));
             }
+            
+            [HttpPost]
+            public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+            {
+                characters.Add(newCharacter);
+                return Ok(characters);
+            }
+
         }
     }
