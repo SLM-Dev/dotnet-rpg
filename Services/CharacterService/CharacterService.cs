6e3ffe096eaa5df207baf72c86b5364cfd9bd49d
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2022-08-11 13:32:42 -0700

    Add Services for Character Add Serfice for Character Interface

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpg_game_dotnet.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        public List<Character> AddCHARACTER(Character newCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
